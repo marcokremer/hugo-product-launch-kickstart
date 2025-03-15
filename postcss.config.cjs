ee0ed15de94793de13e4fb982188066e54b85060
Marco Kremer <kremer.marco@gmail.com>
2025-03-15 13:04:13 +0100

    Fix: PostCSS Konfiguration für Node.js 14 optimiert

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,34 +1,28 @@
-import autoprefixer from 'autoprefixer'
-import postcssImport from 'postcss-import'
-import tailwindcss from 'tailwindcss'
-import purgecss from '@fullhuman/postcss-purgecss'
+const autoprefixer = require('autoprefixer')
+const postcssImport = require('postcss-import')
+const tailwindcss = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
 
-const removeNewlines = (text) => {
-  return text.replace(/(\r\n|\n|\r)/gm, '')
-}
-
-export default {
+module.exports = {
   plugins: [
-    postcssImport,
-    tailwindcss,
-    autoprefixer,
-    ...(process.env.HUGO_ENVIRONMENT === 'production'
-      ? [
-          purgecss({
-            content: ['./hugo_stats.json'],
-            defaultExtractor: (content) => {
-              const els = JSON.parse(content).htmlElements
-              const classes = els.classes || []
-              const ids = els.ids || []
-              const tags = els.tags || []
-              return [...classes, ...ids, ...tags]
-            },
-            safelist: {
-              standard: [/^dark/, /^light/, /^bg-/, /^text-/],
-              deep: [/dark$/, /light$/, /^bg-/, /^text-/],
-            },
-          }),
-        ]
-      : []),
+    postcssImport(),
+    tailwindcss(),
+    autoprefixer(),
+    process.env.HUGO_ENVIRONMENT === 'production'
+      ? purgecss({
+          content: ['./hugo_stats.json'],
+          defaultExtractor: (content) => {
+            const els = JSON.parse(content).htmlElements
+            const classes = els.classes || []
+            const ids = els.ids || []
+            const tags = els.tags || []
+            return [...classes, ...ids, ...tags]
+          },
+          safelist: {
+            standard: [/^dark/, /^light/, /^bg-/, /^text-/],
+            deep: [/dark$/, /light$/, /^bg-/, /^text-/],
+          },
+        })
+      : null,
   ].filter(Boolean),
 } 
