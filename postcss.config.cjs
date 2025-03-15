ac7048ebab3fdb00694959cde05e4447c1f76ff2
Marco Kremer <kremer.marco@gmail.com>
2025-03-15 12:50:44 +0100

    Fix: Node.js und PostCSS Konfiguration aktualisiert

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,6 @@
+const autoprefixer = require('autoprefixer')
 const postcssImport = require('postcss-import')
 const tailwindcss = require('tailwindcss')
-const autoprefixer = require('autoprefixer')
-const postcssPurgecss = require('@fullhuman/postcss-purgecss')
 
 const removeNewlines = (text) => {
   return text.replace(/(\r\n|\n|\r)/gm, '')
@@ -9,24 +8,26 @@ const removeNewlines = (text) => {
 
 module.exports = {
   plugins: [
-    postcssImport(),
-    tailwindcss(),
-    autoprefixer(),
-    process.env.HUGO_ENVIRONMENT === 'production'
-      ? postcssPurgecss({
-          content: ['./hugo_stats.json'],
-          defaultExtractor: (content) => {
-            const els = JSON.parse(content).htmlElements
-            const classes = els.classes || []
-            const ids = els.ids || []
-            const tags = els.tags || []
-            return [...classes, ...ids, ...tags]
-          },
-          safelist: {
-            standard: [/^dark/, /^light/, /^bg-/, /^text-/],
-            deep: [/dark$/, /light$/, /^bg-/, /^text-/],
-          },
-        })
-      : null,
+    postcssImport,
+    tailwindcss,
+    autoprefixer,
+    ...(process.env.HUGO_ENVIRONMENT === 'production'
+      ? [
+          require('@fullhuman/postcss-purgecss')({
+            content: ['./hugo_stats.json'],
+            defaultExtractor: (content) => {
+              const els = JSON.parse(content).htmlElements
+              const classes = els.classes || []
+              const ids = els.ids || []
+              const tags = els.tags || []
+              return [...classes, ...ids, ...tags]
+            },
+            safelist: {
+              standard: [/^dark/, /^light/, /^bg-/, /^text-/],
+              deep: [/dark$/, /light$/, /^bg-/, /^text-/],
+            },
+          }),
+        ]
+      : []),
   ].filter(Boolean),
 } 
